5a63f4dcc8cce7b107e516e6169bcc11e2ab6de7
Andrea De Matteis <andrea.dematteis@cleafy.com>
2022-01-25 12:23:50 +0100

    Added brewing date and minor fixes.

--- app/src/main/java/com/andreadematteis/assignments/beerbox/view/fragments/BeerFragment.kt
+++ app/src/main/java/com/andreadematteis/assignments/beerbox/view/fragments/BeerFragment.kt
@@ -209,14 +209,18 @@ class BeerFragment : Fragment() {
         when (filterPair.first) {
             FilterType.NONE -> {
                 binding.searchText.setText("")
+                binding.searchTextInputLayout.isEnabled = true
             }
 
             FilterType.NAME -> {
                 binding.searchText.setText(filterPair.second)
+                binding.searchTextInputLayout.isEnabled = true
+
             }
 
             FilterType.TIMEFRAME -> {
                 binding.searchText.setText("")
+                binding.searchTextInputLayout.isEnabled = false
                 binding.moreFilters.text = getString(R.string.all_filters_number_label, 1)
 
                 adapter.filterType = FilterType.TIMEFRAME
